6bfdea4981857d9e650fb5f2458e953db64e5703
Ana   Moreira <127451900+AnaMoreeiira@users.noreply.github.com>
2023-09-26 14:21:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import Inicio from './src/screens/Inicio';
+import Drawer from './src/screens/Drawer';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
